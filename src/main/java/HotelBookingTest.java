bbf60e045f353c5f8209dec059ec67b74ca981df
samirsahu35 <34069070+samirsahu35@users.noreply.github.com>
2017-11-29 22:12:23 +0530

    Removed unused package/class imports. Removed blank lines and realligned properly

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -7,47 +7,40 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
+	WebDriver driver = new ChromeDriver();
 
-    WebDriver driver = new ChromeDriver();
+	@FindBy(linkText = "Hotels")
+	private WebElement hotelLink;
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+	@FindBy(id = "Tags")
+	private WebElement localityTextBox;
 
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
+	@FindBy(id = "SearchHotelsButton")
+	private WebElement searchButton;
 
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
+	@FindBy(id = "travellersOnhome")
+	private WebElement travellerSelection;
 
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+	@Test
+	public void shouldBeAbleToSearchForHotels() {
+		setDriverPath();
+		driver.get("https://www.cleartrip.com/");
+		hotelLink.click();
+		localityTextBox.sendKeys("Indiranagar, Bangalore");
+		new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+		searchButton.click();
+		driver.quit();
+	}
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
-
-        driver.quit();
-
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
+	private void setDriverPath() {
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+		}
+	}
 }
--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -6,46 +6,38 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
+	WebDriver driver = new ChromeDriver();
 
-    WebDriver driver = new ChromeDriver();
+	@Test
+	public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+		setDriverPath();
+		driver.get("https://www.cleartrip.com/");
+		waitFor(2000);
+		driver.findElement(By.linkText("Your trips")).click();
+		driver.findElement(By.id("SignIn")).click();
+		driver.findElement(By.id("signInButton")).click();
+		String errors1 = driver.findElement(By.id("errors1")).getText();
+		Assert.assertTrue(errors1.contains("There were errors in your submission"));
+		driver.quit();
+	}
 
-    @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+	private void waitFor(int durationInMilliSeconds) {
+		try {
+			Thread.sleep(durationInMilliSeconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace(); // To change body of catch statement use File | Settings | File Templates.
+		}
+	}
 
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
-
+	private void setDriverPath() {
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+		}
+	}
 }
