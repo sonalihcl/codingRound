63bf37338ede67347b2975b2d6fbd63663c90b28
samirsahu35 <34069070+samirsahu35@users.noreply.github.com>
2017-11-29 22:21:25 +0530

    1) Created the object of Chromedriver inside the test method in order to avoid TestngException. 2) Added to switch to frame to click on Sign in button as its inside the frame.

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -6,15 +6,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver; 
 
 	@Test
 	public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 		setDriverPath();
+		driver= new ChromeDriver();
 		driver.get("https://www.cleartrip.com/");
+		driver.manage().window().maximize();
 		waitFor(2000);
 		driver.findElement(By.linkText("Your trips")).click();
 		driver.findElement(By.id("SignIn")).click();
+		driver.switchTo().frame(driver.findElement(By.id("modal_window")));
 		driver.findElement(By.id("signInButton")).click();
 		String errors1 = driver.findElement(By.id("errors1")).getText();
 		Assert.assertTrue(errors1.contains("There were errors in your submission"));
