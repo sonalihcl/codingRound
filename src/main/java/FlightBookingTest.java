144155d72b56e45b71e5225feeeb437ddc3e308c
samirsahu35 <34069070+samirsahu35@users.noreply.github.com>
2017-11-29 22:37:08 +0530

    1) Instantiate Chromedriver class inside the test method after setdriver() method. 2) Changed the id of teh "To" webelement. 3) Changed the xpath to select the Journey date. 4) Added System.out.println(e.getMessage()+e); to capture the error message with the error to print.

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -9,12 +9,13 @@ import org.testng.annotations.Test;
 import com.sun.javafx.PlatformUtil;
 
 public class FlightBookingTest {
-	WebDriver driver = new ChromeDriver();
-
+	WebDriver driver;
+	
 	@Test
 	public void testThatResultsAppearForAOneWayJourney() {
 		setDriverPath();
-		driver.get("https://www.cleartrip.com/");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		waitFor(2000);
 		driver.findElement(By.id("OneWay")).click();
 		driver.findElement(By.id("FromTag")).clear();
@@ -23,14 +24,14 @@ public class FlightBookingTest {
 		waitFor(2000);
 		List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
 		originOptions.get(0).click();
-		driver.findElement(By.id("toTag")).clear();
-		driver.findElement(By.id("toTag")).sendKeys("Delhi");
+		driver.findElement(By.id("ToTag")).clear();
+		driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 		// wait for the auto complete options to appear for the destination
 		waitFor(2000);
 		// select the first item from the destination auto complete list
 		List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
 		destinationOptions.get(0).click();
-		driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+		driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[3]/td[7]/a")).click();
 		// all fields filled in. Now click on search
 		driver.findElement(By.id("SearchBtn")).click();
 		waitFor(5000);
@@ -44,7 +45,7 @@ public class FlightBookingTest {
 		try {
 			Thread.sleep(durationInMilliSeconds);
 		} catch (InterruptedException e) {
-			e.printStackTrace(); // To change body of catch statement use File | Settings | File Templates.
+			System.out.println(e.getMessage()+e);
 		}
 	}
 
